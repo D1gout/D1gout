35c53b6229dc69aec453c041933ec194eb2f09d3
D1gout <fanolkon@gmail.com>
2022-05-05 00:40:12 +0500

    AutoUpdate

---
+++ Load.bat
@@ -0,0 +1,13 @@
+git add README.md
+
+git add Load.bat
+
+git commit -m "AutoUpdate"
+
+git branch -m main
+
+git remote add origin https://github.com/D1gout/D1gout.git
+
+git push -u origin main
+
+pause
